a174ee6906fc908240fee8272b9717f9d3329277
Nicolas <delton.nicolas@gmail.com>
2023-01-26 22:11:49 +0100

    Ottimizzazione funzioni

--- Snake/main.c
+++ Snake/main.c
@@ -118,69 +118,68 @@ void salva_passo(char dir){
 void verifica_cella(int x0, int y0, char dir){
     if (x+x0 >= 0 && x+x0 < r && y+y0 >= 0 && y+y0 < c){
         if (mappa[x+x0][y+y0] == '$') {
+            salva_passo(dir);
             mappa[x][y] = ' ';
             x += x0;
             y += y0;
             mappa[x][y] = 'o';
             moneta();
-            salva_passo(dir);
         }
         else if (mappa[x+x0][y+y0] == '_') {
+            salva_passo(dir);
             mappa[x][y] = ' ';
             x += x0;
             y += y0;
             mappa[x][y] = 'o';
+            stampa_mappa();
             win();
-            salva_passo(dir);
         }
         else if (mappa[x+x0][y+y0] == 'T') {
+            salva_passo(dir);
             mappa[x][y] = ' ';
             x += x0;
             y += y0;
             mappa[x][y] = 'o';
             trapano();
-            salva_passo(dir);
         }
         else if (mappa[x+x0][y+y0] == '!') {
+            salva_passo(dir);
             mappa[x][y] = ' ';
             x += x0;
             y += y0;
             mappa[x][y] = 'o';
             imprevisto();
-            salva_passo(dir);
         }
         else if (mappa[x+x0][y+y0] == ' ') {
+            salva_passo(dir);
             mappa[x][y] = ' ';
             x += x0;
             y += y0;
             mappa[x][y] = 'o';
-            salva_passo(dir);
         }
     }
 }
 
 void passo(char dir){
-    conta_passi++;
+    
     switch (dir) {
-        case 'n':
-            verifica_cella(-1, 0, dir);
+        case 'N':
+            
             break;
             
-        case 's':
+        case 'S':
             verifica_cella(1, 0, dir);
             break;
             
-        case 'e':
+        case 'E':
             verifica_cella(0, 1, dir);
             break;
             
-        case 'o':
+        case 'O':
             verifica_cella(0, -1, dir);
             break;
     }
-    punteggio--;
-    stampa_mappa();
-    get_move();
+    
 }
 
 void stampa_mappa(void){
@@ -203,32 +202,36 @@ void stampa_mappa(void){
 }
 
 void get_move(void){
+    conta_passi++;
     char c = getchar();
     switch (c) {
         case 'w':
         case 'W':
-            passo('n');
+            verifica_cella(-1, 0, 'N');
             break;
         
         case 's':
         case 'S':
-            passo('s');
+            verifica_cella(1, 0, 'S');
             break;
             
         case 'd':
         case 'D':
-            passo('e');
+            verifica_cella(0, 1, 'E');
             break;
             
         case 'a':
         case 'A':
-            passo('o');
+            verifica_cella(0, -1, 'O');
             break;
             
         default:
             get_move();
             break;
     }
+    punteggio--;
+    stampa_mappa();
+    get_move();
 }
 
 int main(int argc, const char * argv[]) {
