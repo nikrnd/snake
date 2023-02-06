d32f65742de45504fc7cf236c3434b4b8f2b5f9d
Nicolas <delton.nicolas@gmail.com>
2023-02-06 11:01:44 +0100

    last

--- Snake/main.c
+++ Snake/main.c
@@ -8,6 +8,24 @@
 #include <stdlib.h>
 #include <time.h>
 
+#if !defined _WIN32
+#include <termios.h>
+#include <unistd.h>
+
+int _getch(void){
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr(STDIN_FILENO, &oldattr);
+    newattr = oldattr;
+    newattr.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
+    return ch;
+}
+
+#endif
+
 /**Coordinata x della testa**/
 int x;
 /**Coordinata y della testa**/
@@ -350,7 +368,7 @@ void stampa_mappa(void){
  */
 void get_move(void){
     conta_passi++;
-    char c = getchar();
+    char c = _getch();
     switch (c) {
         case 'w':
         case 'W':
