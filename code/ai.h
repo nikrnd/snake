bc4d194e5029ba5601058061c158875ea11550e8
Nicolas <delton.nicolas@gmail.com>
2023-03-28 13:40:37 +0200

    AI parziale (tutto a DX)

--- code/ai.h
+++ code/ai.h
@@ -5,10 +5,12 @@ extern int x; //testa
 extern int y; //testa
 extern int c;
 extern int r;
+extern char* sequenza;
+extern int conta_passi;
 
 void auto_solve(void){
 
-    sleep(1);
+    usleep(200000);
 
     if (y == 0) {
         verifica_cella(0, 1, 'E');
@@ -24,6 +26,24 @@ void auto_solve(void){
         stampa_mappa();
     }
 
-    
+    usleep(200000);
 
+    while (true)
+    {
+        if (sequenza[conta_passi-1] != 'N' && !verifica_cella(1, 0, 'S')) 
+            if (sequenza[conta_passi-1] != 'O' && !verifica_cella(0, 1, 'E'))
+                if (sequenza[conta_passi-1] != 'S' && !verifica_cella(-1, 0, 'N'))
+                    if (sequenza[conta_passi-1] != 'E' && !verifica_cella(0, -1, 'O'))
+                        ;
+
+        stampa_mappa();
+
+        usleep(200000);
+
+        if (mappa[x+1][y] == '_') verifica_cella(-1, 0, 'N');
+        else if (mappa[x-1][y]== '_') verifica_cella(1, 0, 'S');
+        else if (mappa[x][y+1] == '_') verifica_cella(0, 1, 'E');
+        else if (mappa[x][y-1] == '_') verifica_cella(0, -1, 'O');
+    }
+    
 }
